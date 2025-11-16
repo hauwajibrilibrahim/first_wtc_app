8928fa60f0ef8d3a19c4096e3cd0ec4dc5aa2d24
hauwajibrilibrahim <hauwajibrilibrahim@gmail.com>
2025-11-16 16:59:24 +0100

    Enhance app structure with Firebase initialization and add Hospital and Review models

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,31 @@
+import 'dart:io';
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:first_app/bottom_navigation.dart';
 import 'package:first_app/pages/contact_page.dart';
 import 'package:first_app/pages/forgot_password_page.dart';
 import 'package:first_app/pages/login_page.dart';
 import 'package:first_app/pages/onboarding_page.dart';
 import 'package:first_app/pages/signup_page.dart';
+import 'package:first_app/provider/hospital_notifier.dart';
+import 'package:first_app/firebase_options.dart';
+import 'package:first_app/provider/user_notifier.dart';
+import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
-void main() {
+void main()async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  await GoogleSignIn.instance.initialize(
+    clientId: Platform.isAndroid
+        ? "915556525451-8o529t9tt97qmvlihf5ls0gp8io396hn.apps.googleusercontent.com"
+        : "915556525451-b2a1u3ql6a7r07emm1ut75trh18of22a.apps.googleusercontent.com",
+    serverClientId:
+        "915556525451-jlbpidtr6oavum8hu9vnjvbsk89mja2o.apps.googleusercontent.com",
+  );
+
   runApp(const MyApp());
 }
 
@@ -15,21 +34,27 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Save a Life',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+    return ChangeNotifierProvider(
+      create: (context)=> HospitalNotifier(),
+      child: ChangeNotifierProvider(
+        create: (context) => UserNotifier(),
+        child: MaterialApp(
+          title: 'Save a Life',
+          theme: ThemeData(
+            colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          ),
+          debugShowCheckedModeBanner: false,
+          routes: {
+            '/': (context) => OnboardingPage(),
+            '/home': (context) => BottomNavigation(),
+            '/login':(context) => LoginPage(),
+            '/signup': (context) => SignupPage(),
+            '/contact': (context) => ContactPage(),
+            '/forgot': (context) => ForgotPasswordPage()
+          },
+          initialRoute: '/',
+        ),
       ),
-      debugShowCheckedModeBanner: false,
-      routes: {
-        '/': (context) => OnboardingPage(),
-        '/home': (context) => BottomNavigation(),
-        '/login':(context) => LoginPage(),
-        '/signup': (context) => SignupPage(),
-        '/contact': (context) => ContactPage(),
-        '/forgot': (context) => ForgotPasswordPage()
-      },
-      initialRoute: '/',
     );
   }
 }
---
+++ lib/model/hospital.dart
@@ -0,0 +1,32 @@
+
+import 'package:first_app/model/review.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'hospital.g.dart';
+
+@JsonSerializable()
+class Hospital {
+  final String id;
+  final String address;
+  final String name;
+  final String description;
+  final int numberOfAmbulances;
+  final int rating;
+  final String website;
+  final List<String> phoneNumber;
+
+  Hospital({
+    required this.id,
+    required this.address,
+    required this.name,
+    this.description = "",
+    required this.numberOfAmbulances,
+    this.rating = 0,
+    required this.website,
+    required this.phoneNumber,
+  });
+
+  Map<String, dynamic> toJson() => _$HospitalToJson(this);
+
+  factory Hospital.fromJson(Map<String, dynamic> json) => _$HospitalFromJson(json);
+}
