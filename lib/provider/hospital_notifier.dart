29622ee3059fefccf66f7c94bc0fd8678022b4c6
hauwajibrilibrahim <hauwajibrilibrahim@gmail.com>
2025-11-16 17:00:19 +0100

    feat: Implement HospitalNotifier and UserNotifier for managing hospital and user data

---
+++ lib/provider/hospital_notifier.dart
@@ -0,0 +1,27 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:first_app/model/hospital.dart';
+import 'package:flutter/material.dart';
+
+class HospitalNotifier extends ChangeNotifier {
+  List<Hospital> hospitals = [];
+
+  HospitalNotifier(){
+    fetchHospitals();
+  }
+
+  void fetchHospitals() async {
+    QuerySnapshot<Map<String, dynamic>> query = await FirebaseFirestore.instance
+        .collection("hospitals")
+        .get();
+
+    for (QueryDocumentSnapshot<Map<String, dynamic>> doc in query.docs) {
+      Map<String, dynamic> docData = doc.data();
+
+      Hospital hospital = Hospital.fromJson(docData);
+
+      hospitals.add(hospital);
+
+      notifyListeners();
+    }
+  }
+}
---
+++ lib/provider/user_notifier.dart
@@ -0,0 +1,148 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:first_app/model/user_detail.dart';
+import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class UserNotifier extends ChangeNotifier {
+  UserDetail? loggedInUser;
+
+  void login(BuildContext context, String email, String password) async {
+    try {
+      // create user on firebase
+      UserCredential user = await FirebaseAuth.instance
+          .signInWithEmailAndPassword(email: email, password: password);
+
+      //fetch user record from firestore
+      DocumentSnapshot docSnapshot = await FirebaseFirestore.instance
+          .collection("users")
+          .doc(email)
+          .get();
+
+      Map<String, dynamic> data = docSnapshot.data() as Map<String, dynamic>;
+
+      UserDetail userDetail = UserDetail.fromJson(data);
+
+      loggedInUser = userDetail;
+
+      // take user to home page
+      Navigator.of(context).pushReplacementNamed("/home");
+    } on FirebaseAuthException catch (e) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text(e.message ?? "error occured")));
+    } catch (e) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text(e.toString())));
+    }
+    notifyListeners();
+  }
+
+  Future<void> signUp({
+    required BuildContext context,
+    required String userName,
+    required String password,
+    required String email,
+  }) async {
+    try {
+      // create user on firebase auth
+      UserCredential user = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: email, password: password);
+
+      // create userdetail object
+      var userDetail = UserDetail(email: email, name: userName);
+
+      // store user to firestore
+      await FirebaseFirestore.instance
+          .collection("users")
+          .doc(email)
+          .set(userDetail.toJson());
+
+      loggedInUser = userDetail;
+
+      //Alert user on success
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text("User $userName has been created successfully")),
+      );
+
+      // take user to home page
+      Navigator.of(context).pushReplacementNamed("/home");
+    } on FirebaseAuthException catch (e) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text(e.message ?? "error occured")));
+    } catch (e) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text(e.toString())));
+    }
+  }
+
+  void signInWithGoogle(BuildContext context) async {
+    try {
+      var instance = GoogleSignIn.instance;
+      final GoogleSignInAccount? googleUser = await instance.authenticate();
+
+      // Obtain the auth details from the request
+      final GoogleSignInAuthentication googleAuth = googleUser!.authentication;
+
+      // Create a new credential
+      final credential = GoogleAuthProvider.credential(
+        idToken: googleAuth.idToken,
+      );
+
+      // Once signed in, return the UserCredential
+      UserCredential user = await FirebaseAuth.instance.signInWithCredential(
+        credential,
+      );
+
+      // create userdetail object
+      var userDetail = UserDetail(
+        email: user.user!.email!,
+        name: user.user!.displayName ?? "No name",
+        profilePicture: user.user!.photoURL ?? "",
+      );
+
+      // store user to firestore
+      await FirebaseFirestore.instance
+          .collection("users")
+          .doc(userDetail.email)
+          .set(userDetail.toJson());
+
+      loggedInUser = userDetail;
+
+      // take user to home page
+      Navigator.of(context).pushReplacementNamed("/home");
+    } on FirebaseAuthException catch (e) {
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text(e.message ?? "error occured")));
+    } catch (e) {
+      print(e);
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text(e.toString())));
+    }
+  }
+
+  void logout() async {
+    await FirebaseAuth.instance.signOut();
+    loggedInUser = null;
+  }
+
+  void forgotPassword(String email) {
+    FirebaseAuth.instance.sendPasswordResetEmail(email: email);
+  }
+  Future<void> updateUserInfo(UserDetail updatedUser) async {
+  loggedInUser = updatedUser;
+  notifyListeners();
+
+  await FirebaseFirestore.instance
+      .collection("users")
+      .doc(updatedUser.email)
+      .set(updatedUser.toJson());
+}
+
+}
+
